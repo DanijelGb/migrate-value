14330777f4059fba9271e580e276086750df7cd2
DanijelGb <124356150+DanijelGb@users.noreply.github.com>
2024-09-19 11:29:18 +0200

    successfully printed correct value to new file

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Xml.Linq;
+
+XElement doc = XElement.Load("../../../sma_gentext.xml");
+
+IEnumerable<XElement> solution =
+    from el in doc.Element("file")?.Elements("body").Elements("trans-unit")
+    where (string?)el.Attribute("id") == "42007"
+    select el.Element("target");
+
+string path = "../../../target.txt";
+
+foreach (XElement el in solution)
+{
+    File.WriteAllText(path, el.Value);
+}
